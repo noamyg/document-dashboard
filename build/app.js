05bcc89634e08bb8865bbdda85635bf47381e3b7
admin@noamyg.com <noamyg@gmail.com>
2019-07-08 15:48:21 +0300

    Initial Commit

--- build/app.js
+++ build/app.js
@@ -1,47 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-
+var express = require("express");
+var path = require("path");
+var http = require("http");
+var bodyParser = require("body-parser");
+var msp = require("./routes/msp");
 // Get our API routes
 // const api = require('./server/routes/api');
-
-const app = express();
-
+var app = express();
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
-
-// Set our api routes
-// app.use('/api', api);
-
+// Set api routes
+app.use('/data', msp);
 // Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '3000';
+var port = process.env.PORT || '3000';
 app.set('port', port);
-
 /**
  * Create HTTP server.
  */
-const server = http.createServer(app);
-
+var server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
-
-
-
+server.listen(port, function () { return console.log("API running on localhost:" + port); });
 /*
 var express = require('express');
 var path = require('path');
@@ -89,4 +80,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-*/
+*/ 
---
+++ controllers/sqlConnector.ts
@@ -0,0 +1,51 @@
+import sql = require("mssql/msnodesqlv8");
+
+const config = new sql.ConnectionPool({
+    database: "MSP_CAL",
+    server: "localhost",
+    driver: "msnodesqlv8",
+    options: {
+      trustedConnection: true
+    }
+});
+
+var pool: sql.ConnectionPool = null;
+
+module.exports.connect = async () => {
+    try {
+        if (pool) {
+            console.log("Connection pool already exists, returning live pool");
+            return pool;
+        }
+        console.log("Initiating new connection pool");
+        pool = await config.connect();
+        pool.on("error", async (err : Error) => {
+            console.log("Connection pool error");
+        });
+        return pool;
+    }
+    catch ( err ) {
+        console.log("Error connecting to sql server");
+        pool = null;
+    }
+}
+
+module.exports.select = async (query : string) => {
+    try{
+        return await pool.query(query)
+    }
+    catch(err){
+        console.log(`Query failed executing. Nested error: ${err}`)
+    }
+}
+
+export default close = async () => {
+    try {
+        await pool.close();
+        pool = null;
+    }
+    catch ( err ) {
+        pool = null;
+        console.log("Could not close pool");
+    }
+};
